2931f36f4aa6297734cd4617dcbadd71c91ef668
Nicholas Fernandez <Nickfer610@gmail.com>
2022-07-19 19:01:22 +0000

    completed issue 1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,15 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousEntries = localStorage.getItem('previousEntries');
+
+if (previousEntries !== null) {
+  data = JSON.parse(previousEntries);
+}
+
+window.addEventListener('beforeunload', saveEntries);
+
+function saveEntries(event) {
+  localStorage.setItem('previousEntries', JSON.stringify(data));
+}
